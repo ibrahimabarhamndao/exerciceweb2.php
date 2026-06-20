22118c39c13350eb9af4f6c967daa3cdb5032218
Sidy Barham Ndao <ibrahimandao2006@gmail.com>
2026-06-20 15:49:11 +0000

    modif

---
+++ exo5.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+
+?>
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+
+    <label for="prenom">Prenom:</label>
+    <input type="text" name= "prenom">
+    <button>Envoyer</button>    
+    </form>
+</body>
+</html>
